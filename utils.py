eb33802cf487f9f2ec8fbd916b50990f0a3923e8
Tassilo Schweyer <dev@welterde.de>
2011-09-13 23:19:45 +0200

    missed that

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ from pygments import highlight
 from pygments.lexers import get_lexer_for_filename, get_lexer_by_name, \
                             guess_lexer, ClassNotFound
 from pygments.formatters import HtmlFormatter
-from flask import g
+from flask import g, abort
 
 
 
